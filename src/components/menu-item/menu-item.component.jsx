ddb7287687b53318d631ba963009a0aaf76b1162
Gziri <gelagzirishvili@gmail.com>
2021-11-22 02:06:15 +0400

    Fixed V3

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import "./menu-item.styles.scss";
+import { useNavigate, useLocation, useMatch } from "react-router-dom";
 
-import './menu-item.styles.scss';
-
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const match = useMatch(location.pathname);
+  return (
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => navigate(`${match.pathname}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default withRouter(MenuItem);
+export default MenuItem;
