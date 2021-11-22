a1b6de4e79d67d5944d2459d4da9644d7197b265
Gziri <gelagzirishvili@gmail.com>
2021-11-22 14:35:26 +0400

    Added shop
    added shop page, collection preview and collection item components with styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path='/' element={<HomePage/>} />
-        <Route path='/hats' element={<HatsPage/>} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
+      <div></div>
     </div>
   );
 }
