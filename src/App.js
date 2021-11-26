f5c0980f359b66a7dc0ff764e11b5801242f737b
Gziri <gelagzirishvili@gmail.com>
2021-11-26 14:21:09 +0400

    Authentication
    Completed sign up and sign in component in signin page, created authentication service on firebase

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import React from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import SignInAndSIgnUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import './App.css';
 
-import { auth } from "./firebase/firebase.utils";
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-constructor(props){
-  super(props);
+  constructor() {
+    super();
 
-  this.state={
-    currentUser:null
+    this.state = {
+      currentUser: null
+    };
   }
-}
 
-unsibscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
-componentDidMount(){
- this.unsibscribeFromAuth= auth.onAuthStateChanged(user=>{
-    this.setState({currentUser: user})
-    console.log(user)
-  })
-}
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-componentWillUnmount(){
-  this.unsibscribeFromAuth()
-}
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
   render() {
     return (
       <div>
         <Header currentUser={this.state.currentUser} />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndSIgnUpPage />} />
-        </Routes>
-        <div></div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
       </div>
     );
   }
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,25 +1,36 @@
-import React from "react";
-import "./sign-in.styles.scss";
-import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import './sign-in.styles.scss';
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: "",
+      email: '',
+      password: ''
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
@@ -27,31 +38,31 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="sign-in">
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name="email"
-            type="email"
-            handlechange={this.handleChange}
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
             value={this.state.email}
-            label="Email"
+            label='email'
             required
           />
-
           <FormInput
-            name="password"
-            type="password"
-            handlechange={this.handleChange}
+            name='password'
+            type='password'
             value={this.state.password}
-            label="Password"
+            handleChange={this.handleChange}
+            label='password'
             required
           />
-          <div className="buttons">
-            <CustomButton type="submit">Sign In</CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              {""}Sign In With Google{""}
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
@@ -59,4 +70,5 @@ class SignIn extends React.Component {
     );
   }
 }
+
 export default SignIn;
